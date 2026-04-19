0eb4ea5d489bd47eca81f69d236fa79f9632e3c5
engmaireda11 <maireda017@gmail.com>
2026-04-20 01:42:43 +0200

    Add src folder as normal directory (fix submodule issue)

--- src/utiles/utilites.ts
+++ src/utiles/utilites.ts
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 
 export async function getToken(){
     const cookie = await cookies()
-    const token =cookie.get("next-auth.session-token")?.value
+    const token =  cookie.get("__Secure-next-auth.session-token")?.value || cookie.get("next-auth.session-token")?.value;
     const decodedToken =await decode({token:token,secret:process.env.NEXTAUTH_SECRET!})
     return decodedToken?.accesstoken
 }
